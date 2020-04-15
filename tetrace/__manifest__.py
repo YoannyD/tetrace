957e11158f967fd1ef34b7f6e2aba151b7322e03
Adrián <a.delrio@ingetive.com>
2020-04-15 14:24:32 +0200

    Tasas

---
+++ tetrace/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2020 Ingetive - <info@ingetive.com>
+{
+    'name': "Tetrace",
+    'summary': """""",
+    'description': """""",
+    'author': "Ingetive",
+    'website': "http://ingetive.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1.0.1',
+    'depends': [
+        'base',
+        'account_reports'
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
