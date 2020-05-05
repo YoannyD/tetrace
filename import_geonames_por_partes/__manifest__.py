15b2b0c1fcb46ab62b8973a84b9113f05dd625b2
Adrián <a.delrio@ingetive.com>
2020-05-05 13:48:56 +0200

    [ADD] import_geonames_por_partes

---
+++ import_geonames_por_partes/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# © 2020 Ingetive - <info@ingetive.com>
+{
+    'name': "Importar Geonames por partes",
+    'summary': """""",
+    'description': """""",
+    'author': "Ingetive",
+    'website': "http://ingetive.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1.0.1',
+    'depends': [
+        'base_location_geonames_import'
+    ],
+    'data': [
+        'wizard/geonames_import_view.xml'
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
