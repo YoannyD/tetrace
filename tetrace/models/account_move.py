5478766802daedbb9fb2300ac429b01467568f29
Adrián <a.delrio@ingetive.com>
2020-05-18 13:23:51 +0200

    Establecer mecanismo para gestionar los anticipos de facturas

---
+++ tetrace/models/account_move.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# © 2020 Ingetive - <info@ingetive.com>
+
+import logging
+
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    asiento_anticipo_id = fields.Many2one('account.move', domain=[('type', '=', 'entry')], string="Asiento anticipo")
