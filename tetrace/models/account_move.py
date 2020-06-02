06ebd740faeda47a1e6f7e5954598422d6813338
Adrián <a.delrio@ingetive.com>
2020-06-02 10:29:40 +0200

    fecha vencimiento asiento anticipo

--- tetrace/models/account_move.py
+++ tetrace/models/account_move.py
@@ -19,8 +19,10 @@ class AccountMove(models.Model):
     def _compute_fecha_vencimiento_anticipo(self):
         for r in self:
             fecha_vencimiento_anticipo = None
-            for line in r.line_ids:
-                if line.date_maturity and line.account_id and line.account_id.group_id and line.account_id.group_id.code_prefix == 5200:
-                    fecha_vencimiento_anticipo = line.date_maturity
-                    break
+            if r.asiento_anticipo_id:
+                for line in r.asiento_anticipo_id.line_ids:
+                    if line.date_maturity and line.account_id and line.account_id.group_id and \
+                        line.account_id.group_id.code_prefix == 5200:
+                        fecha_vencimiento_anticipo = line.date_maturity
+                        break
             r.fecha_vencimiento_anticipo = fecha_vencimiento_anticipo
