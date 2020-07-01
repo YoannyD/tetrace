430218cda01af69815f51ab1fe76ece3fef5d694
Adrián <a.delrio@ingetive.com>
2020-07-01 12:32:55 +0200

    Cambios Dashboard Proyectos

---
+++ tetrace/models/project_overview.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2020 Ingetive - <info@ingetive.com>
+
+import logging
+
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
+
+
+class Project(models.Model):
+    _inherit = 'project.project'
+
+    def _table_get_empty_so_lines(self):
+        """ get the Sale Order Lines having no timesheet but having generated a task or a project """
+        so_lines = self.sudo()\
+            .mapped('sale_line_id.order_id.order_line')\
+            .filtered(lambda sol: sol.is_service and sol.product_id.service_policy == 'delivered_timesheet' and not sol.is_expense and not sol.is_downpayment)
+        # include the service SO line of SO sharing the same project
+        sale_order = self.env['sale.order'].search([('project_id', 'in', self.ids)])
+        return set(so_lines.ids) | set(sale_order.mapped('order_line').filtered(lambda sol: sol.is_service and sol.product_id.service_policy == 'delivered_timesheet' and not sol.is_expense).ids), set(
+            so_lines.mapped('order_id').ids) | set(sale_order.ids)
