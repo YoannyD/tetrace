8b3d7a25125fc9dcf3de76ee78fa865f09ffbdcd
Adrián <a.delrio@ingetive.com>
2020-07-17 15:51:44 +0200

    botón documentos desde empleado

--- tetrace/models/hr_employee.py
+++ tetrace/models/hr_employee.py
@@ -16,3 +16,20 @@ class Employee(models.Model):
     numero_ss = fields.Char('Nº Seguridad Social')
     IND_NoResidente_A3 = fields.Char('No residente A3')
     sin_adjuntos = fields.Boolean("Sin adjuntos")
+    # documento_url = fields.Char('Documento', compute="_compute_documento_url")
+
+    # def _compute_documento_url(self):
+    #     menu_id = self.env.ref('crm.crm_menu_root').id
+    #     action_id = self.env.ref('crm.action_your_pipeline').id
+    #     for r in self:
+    #         r.url_form = "/web#id=%s&model=crm.lead&view_type=form&menu_id=%s&action=%s" % (r.id, menu_id, action_id)
+
+    def action_view_documentos(self):
+        action = self.env.ref("documents.document_action")
+        action.update({
+            'context': {
+                'default_res_model': 'hr.employee',
+                'res_id': self.id
+            }
+        })
+        return action
