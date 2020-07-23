e038929beb56a811f6e4f196c6bd2cd3532b8cb6
Adrián <a.delrio@ingetive.com>
2020-07-23 13:43:52 +0200

    [ADD] l10n_es_account_bank_statement_import_n43

--- l10n_es_account_bank_statement_import_n43/models/__init__.py
+++ l10n_es_account_bank_statement_import_n43/models/__init__.py
@@ -1,5 +1,3 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import account_bank_statement_import_n43
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import account_journal
---
+++ l10n_es_account_bank_statement_import_n43/readme/CONTRIBUTORS.rst
@@ -0,0 +1,11 @@
+* `Tecnativa <https://www.tecnativa.com>`__:
+
+  * Pedro M. Baeza <pedro.baeza@tecnativa.com>
+
+* `Comunitea <https://www.comunitea.com>`__:
+
+  * Omar Castiñeira Saavedra <omar@comunitea.com>
+
+* `Obertix <https://obertix.net>`__:
+
+  * Vicent Cubells <vicent@vcubells.net>
