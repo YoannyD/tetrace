c4fed94f9725abc00dfbad690ed9aaadb63cd689
Adrián <a.delrio@ingetive.com>
2020-08-07 13:03:27 +0200

    Proyectos, referencia (secuencia) y nombre al confirmar presupuesto

--- tetrace/models/sale_order.py
+++ tetrace/models/sale_order.py
@@ -4,6 +4,7 @@
 import logging
 
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 _logger = logging.getLogger(__name__)
 
@@ -19,3 +20,17 @@ class SaleOrder(models.Model):
     _sql_constraints = [
         ('ref_proyecto_uniq', 'unique (ref_proyecto)', "¡La referencia de proyecto tiene que ser única!"),
     ]
+
+
+class SaleOrderLine(models.Model):
+    _inherit = "sale.order.line"
+
+    def _timesheet_create_project(self):
+        self.ensure_one()
+        if not self.order_id.ref_proyecto or not self.order_id.nombre_proyecto:
+            raise ValidationError("Para crear el proyecto es obligatorio indicar el nombre y la referencia.")
+
+        project = super(SaleOrderLine, self)._timesheet_create_project()
+        name = "%s %s" % (self.order_id.ref_proyecto, self.order_id.nombre_proyecto)
+        project.write({'name': name})
+        return project
