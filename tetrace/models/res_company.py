cc899c5bd6e6ed3a36ee2092a03f2cb9078f9066
Adrián <a.delrio@ingetive.com>
2020-08-13 11:08:18 +0200

    El diario que está en configuración pasarlo a compañia

---
+++ tetrace/models/res_company.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# © 2020 Ingetive - <info@ingetive.com>
+
+import logging
+
+from odoo import models, fields, api
+
+_logger = logging.getLogger(__name__)
+
+
+class Company(models.Model):
+    _inherit = "res.company"
+
+    tetrace_account_move_jorunal_id = fields.Many2one('account.journal', string='Diario de liquidaciones')
