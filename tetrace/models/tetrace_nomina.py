106d1a090760cc8f14e9b984cc953a71fa6f6edf
Adrián <a.delrio@ingetive.com>
2020-09-04 14:50:56 +0200

    nóminas

--- tetrace/models/tetrace_nomina.py
+++ tetrace/models/tetrace_nomina.py
@@ -88,6 +88,9 @@ class Nomina(models.Model):
                 else:
                     self.env['account.move'].create(values)
 
+    def action_generar_distribucion_analitica(self):
+        for r in self:
+            r.nomina_trabajador_ids.generar_distribucion_analitica()
 
 class NominaTrabajador(models.Model):
     _name = 'tetrace.nomina.trabajador'
@@ -158,7 +161,7 @@ class NominaTrabajador(models.Model):
                 ('employee_id', '=', r.employee_id.id),
                 ('date', '>=', r.fecha_inicio),
                 ('date', '<=', r.fecha_fin),
-            ], limit=1)
+            ])
 
             total_horas = 0
             analitica_data = {}
