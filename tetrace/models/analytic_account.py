5248a677b21eb10e15a6f0cef07e7344b4a5bca0
Adrián <a.delrio@ingetive.com>
2020-09-21 15:12:35 +0200

    fix account_id analitica rel

--- tetrace/models/analytic_account.py
+++ tetrace/models/analytic_account.py
@@ -25,6 +25,12 @@ class AccountAnalyticLine(models.Model):
         self._check_imputar_tiempos()
         return super(AccountAnalyticLine, self).write(vals)
 
+    def unlink(self):
+        for line in self:
+            line.line_rel_ids.unlink()
+        res = super(AccountAnalyticLine, self).unlink()
+        return res
+
     def _check_imputar_tiempos(self):
         for r in self:
             if r.task_id and r.task_id.stage_id and r.task_id.stage_id.bloquear_imputar_tiempos:
