62a711bdfe629bb8aee5baac806c5354188d4831
felixvillafranca <f.villafranca@ingetive.com>
2020-10-17 09:19:52 +0000

    Añadimos rango validaciones al usuario

--- tetrace/models/res_users.py
+++ tetrace/models/res_users.py
@@ -7,8 +7,17 @@ from odoo import models, fields, api
 
 _logger = logging.getLogger(__name__)
 
+RANGOS= [
+    ('0', 'Nivel 0'),
+    ('1', 'Nivel 1'),
+    ('2', 'Nivel 2'),
+    ('3', 'Nivel 3'),
+    ('4', 'Nivel 4'),
+    ('5', 'Nivel 5'),
+]
 
 class User(models.Model):
     _inherit = "res.users"
 
     validacion_user_ids = fields.One2many('tetrace.validacion_user', 'user_id')
+    rango_validaciones = fields.Selection(RANGOS, string="Rango")
