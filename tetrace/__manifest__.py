6ad0872b5094aa88a2fa52d0cfb00953a755cfb7
Yoanny Dominguez Carrero <yoannydominguez84@gmail.com>
2022-05-24 10:03:24 -0400

    [FIX] Campo company depent

--- tetrace/__manifest__.py
+++ tetrace/__manifest__.py
@@ -7,7 +7,7 @@
     'author': "Ingetive",
     'website': "https://ingetive.com",
     'category': 'Uncategorized',
-    'version': '13.0.1.0.3',
+    'version': '13.0.1.0.2',
     'depends': [
         'base',
         'sale',
